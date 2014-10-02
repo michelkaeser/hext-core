aa4c89e10cd842d8944d219b2db8fb803d24b31a
Michel Käser <mk@frontender.ch>
2014-10-02 23:48:17 +0200

    I should not assume people dont wanna use the BitsIterator as a standalone class

---
+++ src/lib/io/BitsIterator.hx
@@ -0,0 +1,57 @@
+package lib.io;
+
+import lib.io.Bits;
+
+/**
+ * Iterator class to traverse each Bit of a Bits abstract.
+ *
+ * @see http://api.haxe.org/Iterator.html
+ */
+class BitsIterator
+{
+    /**
+     * Stores the Bits over which we iterate.
+     *
+     * @var lib.io.Bits
+     */
+    private var bits:Bits;
+
+    /**
+    * Stores current index/position.
+    *
+    * @var Int
+    */
+    private var position:Int;
+
+
+    /**
+     * Constructor to initialize a new BitsIterator instance.
+     *
+     * @param lib.io.Bits bits the Bits to iterate over
+     */
+    public function new(bits:Bits):Void
+    {
+        this.bits     = bits;
+        this.position = 0;
+    }
+
+    /**
+     * Checks if there is another bit remaining.
+     *
+     * @return Bool
+     */
+    public inline function hasNext():Bool
+    {
+        return this.position < (this.bits.length << 3);
+    }
+
+    /**
+     * Returns the next Bit.
+     *
+     * @return lib.io.Bit
+     */
+    public inline function next():Bit
+    {
+        return this.bits[this.position++];
+    }
+ }
